4fe175604102cb6608e066bed49410738f706375
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-09 17:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // complete this js code
-function Person(name, age) {}
+// Person class
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Adding greet method to Person prototype
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee class inheriting from Person
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor to initialize name and age
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inheriting Person prototype into Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Adding jobGreet method to Employee prototype
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
