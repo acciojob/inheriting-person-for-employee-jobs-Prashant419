bb11528556d40c2bc09713032d1f8a179476f37b
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-09 17:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 
-  this.greet = function () {
+  this.greet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
   };
 }
 
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor using `call`
-  Person.call(this, name, age);
+  // Call the Person constructor using `super`
+  super(name, age);
 
   this.jobTitle = jobTitle;
 
-  this.jobGreet = function () {
+  this.jobGreet = function() {
     console.log(
       `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
     );
